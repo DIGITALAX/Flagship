db2b8ad67a0965fec74b4f6da2c4df00b9a4cfa9
emmajane1313 <admin@digitalax.xyz>
2024-05-13 23:08:22 -0400

    graph se actualizo

--- lib/sub/client.ts
+++ lib/sub/client.ts
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+
 const httpLinkPrint = new HttpLink({
-  uri: "https://api.thegraph.com/subgraphs/name/digitalax/print-library",
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/DcuUkg3QC5zg1t86VeNjWzg6R6ohaGa8QGyVE1rFYMZB`,
 });
+
 
 export const graphPrintClient = new ApolloClient({
   link: httpLinkPrint,
@@ -10,7 +12,7 @@ export const graphPrintClient = new ApolloClient({
 });
 
 const httpLinkKinora = new HttpLink({
-  uri: "https://api.thegraph.com/subgraphs/name/digitalax/kinora",
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/DKi4s9USksFKEobZaofL3QAST4SQGhuEtsBJcBY4Dn8`,
 });
 
 export const graphKinoraClient = new ApolloClient({
