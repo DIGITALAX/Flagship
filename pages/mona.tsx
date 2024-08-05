e4ba7224eca20a3b3d1a6e55c3860be49a2869e9
emmajane1313 <admin@digitalax.xyz>
2024-08-05 09:00:21 -0400

    nuevo

--- pages/mona.tsx
+++ pages/mona.tsx
@@ -17,6 +17,15 @@ const Mona: NextPage<MonaProps> = ({ router, rewind }): JSX.Element => {
       className="flex flex-col bg-gradient-to-b from-windows to-white justify-start items-center h-[400rem] min-h-full gap-20"
       ref={rewind}
     >
+      <div className="absolute w-full h-full top-0 left-0">
+        <Image
+          src={`${INFURA_GATEWAY}/ipfs/QmXwwJBRScviuKgV1HrhU8GeFEVckadoGA7GugM3BFV6Yz`}
+          objectFit="cover"
+          layout="fill"
+          draggable={false}
+        />
+        <div className="relative w-full h-full flex bg-windows/60"></div>
+      </div>
       <Curva
         tipo={tipo}
         textColors={textColors}
@@ -34,12 +43,20 @@ const Mona: NextPage<MonaProps> = ({ router, rewind }): JSX.Element => {
         </div>
       </div>
       <div className="relative w-full flex flex-col items-center justify-center h-[50rem] text-center text-white gap-px font-bit text-2xl">
-        <div className="relative w-fit h-fit flex text-3xl pb-3 text-black break-all">
+        <div className="relative w-[25rem] h-[25rem] flex items-center justify-center">
+          <Image
+            src={`${INFURA_GATEWAY}/ipfs/QmakB3FwKhkhEUib9uGjRoPii2CX3h8BmaEjmkGpQtA6G7`}
+            draggable={false}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="pt-4 relative w-fit h-fit flex text-3xl pb-3 text-white break-all text-sm">
           {t("modo")}
         </div>
         <div
           className={`relative w-fit h-fit flex cursor-sewingHS active:scale-95 hover:text-lightYellow break-all ${
-            tipo ? "text-lightYellow" : "text-white"
+            tipo ? "text-lightYellow underline" : "text-white"
           }`}
           onClick={() => setTipo(true)}
         >
@@ -48,19 +65,11 @@ const Mona: NextPage<MonaProps> = ({ router, rewind }): JSX.Element => {
         <div className="relative w-fit h-fit flex"> {t("o")}</div>
         <div
           className={`relative w-fit h-fit flex cursor-sewingHS active:scale-95 hover:text-lightYellow break-all  ${
-            !tipo ? "text-lightYellow" : "text-white"
+            !tipo ? "text-lightYellow underline" : "text-white"
           }`}
           onClick={() => setTipo(false)}
         >
           {t("espectador")}
-        </div>
-        <div className="relative w-96 h-96 flex items-center justify-center">
-          <Image
-            src={`${INFURA_GATEWAY}/ipfs/QmdKiSJPQauP9LJk59HQAcHnUktr4JbuTrKTgyvzNFt5av`}
-            draggable={false}
-            layout="fill"
-            objectFit="contain"
-          />
         </div>
       </div>
       <div className="relative flex flex-col items-center justify-center h-fit text-center text-white font-bit">
