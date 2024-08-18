1123d06e8acdb1cfcb972685859b5a2ed0d53452
emmajane1313 <admin@digitalax.xyz>
2024-08-17 22:31:17 -0400

    next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -229,7 +229,7 @@ function MyApp({ Component, pageProps }: AppProps) {
               <Modals />
               <div style={{ display: "none" }}>
                 Web3, Web3 Fashion, Moda Web3, Open Source, CC0, Emma-Jane
-                MacKinnon-Lee, Open Source LLMs, DIGITALAX
+                MacKinnon-Lee, Open Source LLMs, DIGITALAX, F3Manifesto, www.digitalax.xyz, www.f3manifesto.xyz, Women, Life, Freedom.
               </div>
             </div>
           </Provider>
