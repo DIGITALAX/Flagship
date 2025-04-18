c2272c35e2157fdcf875e17fc2e1e2af7067de01
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-17 20:54:32 -0400

    user binding

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -19,6 +21,53 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              context: ".next/server",
+              to: "./app/[name][ext]",
+              from: path.resolve(
+                __dirname,
+                "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node"
+              ),
+              filter: (resourcePath: string) => resourcePath.endsWith(".wasm"),
+            },
+          ],
+        }),
+        new CopyPlugin({
+          patterns: [
+            {
+              context: ".next/server",
+              to: "./chunks/[name][ext]",
+              from: path.resolve(
+                __dirname,
+                "node_modules/@xmtp/user-preferences-bindings-wasm/dist/node"
+              ),
+              filter: (resourcePath: string) => resourcePath.endsWith(".wasm"),
+            },
+          ],
+        })
+      );
+    }
+
+
+
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true,
+    };
+
+    if (!isServer) {
+      config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+    }
+
+    return config;
+  },
+
   trailingSlash: true,
   async headers() {
     let headersConfig: any[] = [];
